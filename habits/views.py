9eec41508571ac3f5b7d079dd4b0245f630fae3d
Sheen <dan.80956@gmail.com>
2023-11-18 16:18:56 +0300

    add views and urls in habits

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from habits.models import Habit
+from habits.serializers import HabitSerializer
+from habits.permissions import IsOwnerOrReadOnly, CanReadPublicHabits
+
+
+class HabitViewSet(viewsets.ModelViewSet):
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    permission_classes = [IsOwnerOrReadOnly]
+    # pagination_class =
+
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+
+class PublicHabitViewSet(viewsets.ModelViewSet):
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.filter(is_public=True)
+    permission_classes = [CanReadPublicHabits]
